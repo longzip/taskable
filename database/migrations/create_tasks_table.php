7f504330f2cb5a58b758ebccbd8f385958865b97
noithatzip <longlv@v-auto.com.vn>
2019-04-13 15:22:28 +0700

    first commit

---
+++ database/migrations/create_tasks_table.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Kalnoy\Nestedset\NestedSet;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->tinyInteger('priority')->nullable();
+            $table->date('duedate')->nullable();
+            $table->boolean('status')->default(0);
+            $table->morphs('tasklog');
+            $table->morphs('creator');
+            NestedSet::columns($table);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
